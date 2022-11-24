f8ef6971f806b97a77edeea7ea8b2f5277d7b67f
Raymon Roos <raymon.roos@hotmail.com>
2022-11-24 09:49:57 +0100

    feat: added cache/fetch logic to sw

--- src/sw.js
+++ src/sw.js
@@ -5,18 +5,44 @@ self.addEventListener('install', (e) => {
 const cacheName = 'mrfix-cache';
 
 const appShellFiles = [
-    './app.js',
-    './components/navbar.html',
-    './css/input.css',
-    './icons/back_arrow.svg',
-    './icons/booking.svg',
-    './icons/chat.svg',
-    './icons/mrfix-logo-en.svg',
-    './icons/notification_bell.svg',
-    './icons/profile.svg',
-    './icons/request.svg',
-    './pages/index.html',
-    './pages/job_offer.html',
-    './pages/profile.html',
-    './sw.js',
+    'dist/output.css',
+    'manifest.webmanifest',
+    'src/app.js',
+    'src/components/navbar.html',
+    'src/css/input.css',
+    'src/icons/back_arrow.svg',
+    'src/icons/booking.svg',
+    'src/icons/chat.svg',
+    'src/icons/mrfix-logo-en.svg',
+    'src/icons/notification_bell.svg',
+    'src/icons/profile.svg',
+    'src/icons/request.svg',
+    'src/pages/index.html',
+    'src/pages/job_offer.html',
+    'src/pages/profile.html',
+    'src/sw.js',
 ];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+
+        if (r) { return r; }
+
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
+});
