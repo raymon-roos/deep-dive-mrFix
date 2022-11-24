e7185448cf11a05f310bc449e71b05a602be5f24
Raymon Roos <raymon.roos@hotmail.com>
2022-11-24 12:01:11 +0100

    feat: different login redirects for fixers/customers
    NOTE: navigation is still broken, will require proper javascript

--- src/login.js
+++ src/login.js
@@ -1,11 +1,15 @@
 function validateForm() {
-  let pass = document.forms["login"]["pass"].value;
-  let email = document.forms["login"]["email"].value;
-	console.log(email.length);
-  if (pass == "password" && email.length > 0) {
-		window.location = "./index.html";
-		return true;
-  }
-    alert("log in failed");
+    const pass = document.forms.login.pass.value;
+    const email = document.forms.login.email.value;
+    if (pass == 'password' && email.length > 0) {
+        window.location = './job_offer.html#screen1';
+        return true;
+    }
+
+    if (pass == 'fixer' && email.length > 0) {
+        window.location = './job_accepter.html#screen1';
+        return true;
+    }
+    alert('log in failed');
     return false;
-} 
+}
