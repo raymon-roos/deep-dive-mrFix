7b79ae5726951462e128b031df4dbd4d0fe36b7a
Raymon Roos <raymon.roos@hotmail.com>
2022-11-24 10:57:56 +0100

    fix: removed redundant console.log

--- src/login.js
+++ src/login.js
@@ -1,7 +1,6 @@
 function validateForm() {
   let pass = document.forms["login"]["pass"].value;
   let email = document.forms["login"]["email"].value;
-	console.log(email.length);
   if (pass == "password" && email.length > 0) {
 		window.location = "./index.html";
 		return true;
